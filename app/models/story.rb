a1df802caa561db07ca23687d1bf38d25a92939b
Phil Monroe <phil@philmonroe.com>
2014-03-27 23:37:17 -0700

    better overview

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,10 @@
 class Story < ActiveRecord::Base
   belongs_to :backlog
 
+  def overview
+    ["As a #{who}", "I want #{what}", "So that #{why}"].join(?\n)
+  end
+
   def roi
     self.business_value.to_i / self.story_points.to_i
   rescue
