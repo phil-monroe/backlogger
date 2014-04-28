669849f48f3bfdea0f5983c96f006de9c1a0928a
Phil Monroe <phil@philmonroe.com>
2014-04-28 17:19:48 -0400

    ability to "share" backlogs at the model layer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
 
   has_many :backlogs
 
+  has_many :backlog_sharings
+  has_many :shared_backlogs, through: :backlog_sharings, source: :backlog
+
   def self.from_google_omniauth(auth_hash)
     self.find_or_initialize_by(google_uid: auth_hash[:uid]).tap do |user|
       user.first_name ||= auth_hash.info.first_name
